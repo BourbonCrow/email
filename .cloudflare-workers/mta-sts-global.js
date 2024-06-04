06104d5c146c3dd3d2c99f246c93998d229da616
BourbonCrow <mattin.br@gmail.com>
2024-06-05 00:58:30 +0200

    Update mta-sts-global.js

--- .cloudflare-workers/mta-sts-global.js
+++ .cloudflare-workers/mta-sts-global.js
@@ -1,8 +1,11 @@
-// This worker is designed to be able to neatly handle MTA-STS policies for multiple domains that use the exact same txt records for example Proton Mail using own domain.
+// This worker is designed to be able to neatly handle MTA-STS policies for multiple domains.
 
 // Make a new worker with this script and add your domains to the stsPolicies dict like the example.
 // Add a DNS AAAA record for mta-sts.yourdomain.com pointing to 100:: and set to proxied,
 // then add a workers route for mta-sts.yourdomain.com/* pointing to this worker.
+
+// You should probably also create a Cloudflare configuration rule disabling Browser Integrity Check for the mta-sts subdomain
+// to ensure MTAs aren't blocked from retrieving your policy.
 
 // You'll still need to manually add the appropriate _mta-sts.yourdomain.com TXT record to enable the policy, 
 // and the _smtp._tls.yourdomain.com TXT record for reporting.
